1c2429bbe4ea599f649bc6df9eaeeb10b146d020
Žane Suhadolnik <zanesuhi@gmail.com>
2022-12-07 10:53:33 +0100

    add icons, background, toolbar

---
+++ src/components/common/toolbar.tsx
@@ -0,0 +1,119 @@
+import * as React from 'react';
+import styled from '@emotion/styled';
+
+import iconAperture from '../../assets/icons/aperture.svg';
+import iconFolder from '../../assets/icons/folder.svg';
+import iconChrome from '../../assets/icons/chrome.svg';
+import iconRss from '../../assets/icons/rss.svg';
+import iconCamera from '../../assets/icons/camera.svg';
+import iconImage from '../../assets/icons/image.svg';
+import iconSearch from '../../assets/icons/search.svg';
+import iconWifi from '../../assets/icons/wifi.svg';
+import iconBattery from '../../assets/icons/battery.svg';
+import iconBell from '../../assets/icons/bell.svg';
+
+interface ToolbarProps {
+	modals: any;
+}
+
+const MODAL_NAMES_MAP = {
+	fileDirectoryModal: 'File Directory',
+	rssReaderModal: 'RSS Reader',
+	cameraModal: 'Camera',
+	galleryModal: 'Gallery',
+	browserModal: 'Browser',
+};
+function Toolbar({ modals }: ToolbarProps) {
+	const [date, setDate] = React.useState(new Date());
+	const [openedModal, setOpenedModal] = React.useState(null);
+
+	React.useEffect(() => {
+		const timerId = setInterval(refreshClock, 1000);
+		return () => clearInterval(timerId);
+	}, []);
+
+	React.useEffect(() => {
+		for (const [key, value] of Object.entries(modals)) {
+			if (value) setOpenedModal(MODAL_NAMES_MAP[key]);
+		}
+		if (Object.values(modals).every((value) => !value)) setOpenedModal(null);
+	}, [modals]);
+
+	const refreshClock = () => {
+		setDate(new Date());
+	};
+
+	return (
+		<Wrapper>
+			<div style={{ display: 'flex', alignItems: 'center', gap: 20 }}>
+				<img src={iconAperture} />
+				<img src={iconSearch} />
+				{openedModal && (
+					<Opened>
+						{(() => {
+							switch (openedModal) {
+								case 'File Directory':
+									return <img src={iconFolder} />;
+								case 'RSS Reader':
+									return <img src={iconRss} />;
+								case 'Camera':
+									return <img src={iconCamera} />;
+								case 'Gallery':
+									return <img src={iconImage} />;
+								case 'Browser':
+									return <img src={iconChrome} />;
+							}
+						})()}
+						{openedModal}
+					</Opened>
+				)}
+			</div>
+			<RightSideWrapper>
+				<div>
+					<img src={iconBell} />
+				</div>
+				<div>
+					<img src={iconWifi} />
+				</div>
+				<div>
+					<img src={iconBattery} />
+				</div>
+				<p style={{ fontFamily: 'Roboto, sans-serif' }}>
+					{date.toLocaleTimeString()}
+				</p>
+			</RightSideWrapper>
+		</Wrapper>
+	);
+}
+
+const Wrapper = styled.div({
+	fontFamily: 'Roboto, sans-serif',
+	backgroundColor: '#171614',
+	color: 'white',
+	maxWidth: '100%',
+	height: '5vh',
+
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'space-between',
+	padding: '0 20px',
+});
+
+const Opened = styled.div({
+	width: 150,
+	border: '1px solid white',
+	borderRadius: 8,
+	padding: 10,
+
+	display: 'flex',
+	alignItems: 'center',
+	gap: 10,
+});
+
+const RightSideWrapper = styled.div({
+	display: 'flex',
+	alignItems: 'center',
+	gap: 10,
+});
+
+export default Toolbar;
--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -1,7 +1,117 @@
-import * as React from "react";
+import * as React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from '@emotion/styled';
 
-const Home = () => {
-  return <div>Hello</div>;
-};
+import background from '../../assets/img/background.jpeg';
+import iconFolder from '../../assets/icons/folder.svg';
+import iconChrome from '../../assets/icons/chrome.svg';
+import iconRss from '../../assets/icons/rss.svg';
+import iconCamera from '../../assets/icons/camera.svg';
+import iconImage from '../../assets/icons/image.svg';
+
+function Home() {
+	const [fileDirectoryModal, setFileDirectoryModal] = React.useState(false);
+	const [rssReaderModal, setRssReaderModal] = React.useState(false);
+	const [cameraModal, setCameraModal] = React.useState(false);
+	const [galleryModal, setGalleryModal] = React.useState(false);
+	const [browserModal, setBrowserModal] = React.useState(false);
+	const navigate = useNavigate();
+
+	React.useEffect(() => {
+		const authToken = localStorage.getItem('Email');
+		if (authToken) navigate('/');
+		else navigate('/login');
+	}, [navigate]);
+
+	return (
+		<>
+			<Desktop>
+				<IconsWrapper>
+					<Icon onClick={() => setFileDirectoryModal(true)}>
+						<img src={iconFolder} width={'50'} />
+						<IconTitle>File Directory</IconTitle>
+					</Icon>
+					<Icon onClick={() => setRssReaderModal(true)}>
+						<img src={iconRss} width={'50'} />
+						<IconTitle>RSS Reader</IconTitle>
+					</Icon>
+					<Icon onClick={() => setCameraModal(true)}>
+						<img src={iconCamera} width={'50'} />
+						<IconTitle>Camera</IconTitle>
+					</Icon>
+					<Icon onClick={() => setGalleryModal(true)}>
+						<img src={iconImage} width={'50'} />
+						<IconTitle>Gallery</IconTitle>
+					</Icon>
+					<Icon onClick={() => setBrowserModal(true)}>
+						<img src={iconChrome} width={'50'} />
+						<IconTitle>Chrome</IconTitle>
+					</Icon>
+				</IconsWrapper>
+			</Desktop>
+			{/*<Toolbar
+				modals={{
+					fileDirectoryModal,
+					rssReaderModal,
+					cameraModal,
+					galleryModal,
+					browserModal,
+				}}
+			/>*/}
+			{/*<FileDirectoryModal
+      isOpen={fileDirectoryModal}
+      onClose={() => setFileDirectoryModal(false)}
+    />
+    <RssReaderModal
+      isOpen={rssReaderModal}
+      onClose={() => setRssReaderModal(false)}
+    />
+    <CameraModal isOpen={cameraModal} onClose={() => setCameraModal(false)} />
+    <GalleryModal
+      isOpen={galleryModal}
+      onClose={() => setGalleryModal(false)}
+    />
+    <BrowserModal
+      isOpen={browserModal}
+      onClose={() => setBrowserModal(false)}
+    />*/}
+		</>
+	);
+}
+
+const Desktop = styled.div({
+	fontFamily: 'Roboto, sans-serif',
+	backgroundImage: `url(${background})`,
+	backgroundRepeat: 'no-repeat',
+	maxWidth: '100%',
+	height: '95vh',
+
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'flex-start',
+});
+
+const IconsWrapper = styled.div({
+	color: 'white',
+	height: '60%',
+	display: 'flex',
+	flexDirection: 'column',
+	alignItems: 'center',
+	justifyContent: 'space-between',
+	padding: 10,
+	gap: '2em',
+});
+
+const Icon = styled.div({
+	display: 'flex',
+	flexDirection: 'column',
+	alignItems: 'center',
+	cursor: 'pointer',
+});
+
+const IconTitle = styled.div({
+	marginTop: 5,
+	textShadow: 'black 1px 0 10px',
+});
 
 export default Home;
